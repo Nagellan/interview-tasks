fe879ef3aad31b4d0c8d3ad6152da6cfe0ea8743
HardLight00 <imazan00@gmail.com>
2019-11-03 03:30:51 +0300

    Refactor fibonacci

--- fibonacci.js
+++ fibonacci.js
@@ -2,13 +2,11 @@
 print N-th fibonacci number
  */
 
-const fibonacci = (num, fib1 = 0, fib2 = 1) => {
-    if (num === 0)
-        return fib1;
-    if (num === 1)
-        return fib2;
-    return fibonacci(num - 1, fib2, fib1 + fib2);
+const fibonacci = (num, pre = 0, accum = 1) => {
+    if (num < 2)
+        return accum;
+    return fibonacci(num - 1, accum, pre + accum);
 };
 
-for (let i = 0; i < 20; i++)
-    console.log(fibonacci(i));
+for (let i = 1; i < 20; i++)
+    console.log(i, ':', fibonacci(i));
